9153fef0701f246b6470b55e56e0e5d71bf26d7c
EnricMateu <xaxeu@hotmail.com>
2019-08-21 14:46:21 +0200

    se añaden detalles al primer test, dejandolo funcional

--- tests/SacrificeTest.php
+++ tests/SacrificeTest.php
@@ -7,11 +7,13 @@ final class SacrificeTest extends TestCase
     public function testIfSacrificedChangesStatus()
     {
     //Definimos el escenario del test. (estado inicial, sacrificado False) 
-
+    $sacrifice = new Sacrifice ();
+    $this->assertFalse($sacrifice->sacrified);
     // Ejecutamos el escenario
+    $sacrifice->execute(); 
 
     // Comprobamos el resultado
-
+    $this->assertTrue($sacrifice->sacrified);
     }
 }
 ?>
